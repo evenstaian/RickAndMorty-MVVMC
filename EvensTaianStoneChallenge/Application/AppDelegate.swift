5886ab2a878ac0c221e42f3b73feb9209d433624
Evens Taian <evenstaian@gmail.com>
2023-08-15 02:30:01 -0300

    unable url cache

--- EvensTaianStoneChallenge/Application/AppDelegate.swift
+++ EvensTaianStoneChallenge/Application/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window!.rootViewController = navigation
         window!.makeKeyAndVisible()
         
-        //TaskStore.makeCache()
+        TaskStore.makeCache()
         return true
     }
 
